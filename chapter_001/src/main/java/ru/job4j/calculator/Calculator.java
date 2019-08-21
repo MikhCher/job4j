2b9c70335ff1238c32e2f5b097e0ef6dc28f4fd2
Mikhail Cherednichenko <28explay@gmail.com>
2019-08-21 19:22:20 +0300

    5.7. JavaDoc. Code convention[#170189]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,25 +1,67 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Класс для вычисления арифметических операций + - / *
+ * @author Mikhail Cherednichenko (28explay@gmail.com)
+ * @since  21.08.2019
+ * @version 1
+ */
 public class Calculator {
+    /**
+     * Сложение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void add(double first, double second) {
+        /**
+         * результат
+         */
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
+    /**
+     * Деление
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void div(double first, double second) {
+        /**
+         * результат
+         */
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
+    /**
+     * Умножение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void multiply(double first, double second) {
+        /**
+         * результат
+         */
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
+    /**
+     * Вычитание
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void subtrack(double first, double second) {
+        /**
+         * результат
+         */
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     * Main
+     * @param args аргументы
+     */
     public static void main(String[] args) {
-        add(1, 1);
+        add(1, 1);  
         div(4, 2);
         multiply(2, 1);
         subtrack(10, 5);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Mikhail Cherednichenko (28explay@gmail.com)
+* @version 1
+* @since 21.08.2019
+*/
+package ru.job4j.calculator;
