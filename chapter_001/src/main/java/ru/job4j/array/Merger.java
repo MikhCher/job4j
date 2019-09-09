d642500918a67efc333257cdcae7b638e1120d89
Mikhail Cherednichenko <28explay@gmail.com>
2019-09-09 18:33:33 +0300

    Экзамен. Базовый синтаксис.[#170231]

--- chapter_001/src/main/java/ru/job4j/array/Merger.java
+++ chapter_001/src/main/java/ru/job4j/array/Merger.java
@@ -3,27 +3,25 @@ package ru.job4j.array;
 public class Merger {
     public static int[] merge(int[] first, int[] second) {
         int[] third = new int[20];
-        for (int i = 0; i < first.length; i++) {
-            int j = i * 2;
-            if (first[i] < second[i]) {
-                if (j != 0 && third[j - 1] > first[i]) {
-                    int tmp = third[j - 1];
-                    third[j - 1] = first[i];
-                    third[j] = tmp;
-                } else {
-                    third[j] = first[i];
-                }
+        for (int i = 0, j = 1; i < first.length; i++) {
+            if (first[i] <= second[i] && first[i] >= third[j - 1]) {
+                third[i * 2] = first[i];
+                third[i * 2 + 1] = second[i];
+            } else if (first[i] < second[i] && first[i] <= third[j - 1]) {
+                int tmp = third[j - 1];
+                third[j - 1] = first[i];
+                third[j] = tmp;
                 third[j + 1] = second[i];
+            } else if (second[i] < first[i] && second[i] >= third[j - 1]) {
+                third[i * 2] = second[i];
+                third[i * 2 + 1] = first[i];
             } else {
-                if (j != 0 && third[j - 1] > second[i]) {
-                    int tmp = third[j - 1];
-                    third[j - 1] = second[i];
-                    third[j] = tmp;
-                } else {
-                    third[j] = second[i];
-                }
+                int tmp = third[j - 1];
+                third[j - 1] = second[i];
+                third[j] = tmp;
                 third[j + 1] = first[i];
             }
+            j += j == 1 ? 1 : 2;
         }
         return third;
     }
