21f6faf5ba3eb4d50b53daf5c10d2e8789527565
Mikhail Cherednichenko <28explay@gmail.com>
2019-09-12 17:32:25 +0300

    1.5. Котенок гав и его котлета[#170313]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,32 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("This is the name and food of the first cat:");
+        Cat first = new Cat();
+        first.giveNick("Gav");
+        first.eat("kotleta");
+        first.show();
+        System.out.println("This is the name and food of the second cat:");
+        Cat second = new Cat();
+        second.giveNick("Alex");
+        second.eat("fish");
+        second.show();
     }
 }
