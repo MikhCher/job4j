a16c03c58d29077c9c5bc461daf90642c1c74547
Mikhail Cherednichenko <28explay@gmail.com>
2019-09-28 18:09:21 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#170291]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,13 +1,19 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
+    private int counter = 0;
+    private String[] choice = new String[10];
+
     @Test
     public void whenAddItem() {
         String[] answers = {"Fix PC"};
@@ -48,12 +54,17 @@ public class StartUITest {
         new StartUI().init(input, new Tracker(), new UserActions[] { action });
         assertThat(action.isCall(), is(true));
     }
+    @Before
+    public void before() {
+
+    }
     @Test
     public void whenAdd() {
         StubInput input = new StubInput(
                 new String[] {"0", "Add test", "1"}
         );
         CreateAction action = new CreateAction();
+        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -63,6 +74,7 @@ public class StartUITest {
                 new String[] {"0", "1"}
         );
         ShowAction action = new ShowAction();
+        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -73,6 +85,7 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(rm.nextLong() + System.currentTimeMillis()), "Edit test", "1"}
         );
         EditAction action = new EditAction();
+        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -83,6 +96,7 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(rm.nextLong() + System.currentTimeMillis()), "1"}
         );
         DeleteAction action = new DeleteAction();
+        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -93,6 +107,7 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(rm.nextLong() + System.currentTimeMillis()), "1"}
         );
         FindByIDAction action = new FindByIDAction();
+        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -102,7 +117,18 @@ public class StartUITest {
                 new String[] {"0", "Random name", "1"}
         );
         FindByNameAction action = new FindByNameAction();
+        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
+    }
+    @After
+    public void after() {
+        String[] array = new String[counter];
+        System.arraycopy(choice, 0, array, 0, counter);
+        System.out.println("\n\nUser-selected menu items: ");
+        for (String i: array) {
+            System.out.println(i);
+        }
+        System.out.println("Exit");
     }
 }
