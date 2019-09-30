2e891910c95af55cbc7cd2f58bea113d16d722b9
Mikhail Cherednichenko <28explay@gmail.com>
2019-09-30 17:07:11 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#170291]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,18 +1,19 @@
 package ru.job4j.tracker;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import java.util.StringJoiner;
 import java.util.Random;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-    private int counter = 0;
-    private String[] choice = new String[10];
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
     @Test
     public void whenAddItem() {
@@ -54,17 +55,12 @@ public class StartUITest {
         new StartUI().init(input, new Tracker(), new UserActions[] { action });
         assertThat(action.isCall(), is(true));
     }
-    @Before
-    public void before() {
-
-    }
     @Test
     public void whenAdd() {
         StubInput input = new StubInput(
                 new String[] {"0", "Add test", "1"}
         );
         CreateAction action = new CreateAction();
-        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -74,7 +70,6 @@ public class StartUITest {
                 new String[] {"0", "1"}
         );
         ShowAction action = new ShowAction();
-        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -85,7 +80,6 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(rm.nextLong() + System.currentTimeMillis()), "Edit test", "1"}
         );
         EditAction action = new EditAction();
-        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -96,7 +90,6 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(rm.nextLong() + System.currentTimeMillis()), "1"}
         );
         DeleteAction action = new DeleteAction();
-        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -107,7 +100,6 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(rm.nextLong() + System.currentTimeMillis()), "1"}
         );
         FindByIDAction action = new FindByIDAction();
-        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
@@ -117,18 +109,56 @@ public class StartUITest {
                 new String[] {"0", "Random name", "1"}
         );
         FindByNameAction action = new FindByNameAction();
-        choice[counter++] = action.name();
         new StartUI().init(input, new Tracker(), new UserActions[] { action, new ExitAction() });
         assertThat(action.isCall(), is(true));
     }
-    @After
-    public void after() {
-        String[] array = new String[counter];
-        System.arraycopy(choice, 0, array, 0, counter);
-        System.out.println("\n\nUser-selected menu items: ");
-        for (String i: array) {
-            System.out.println(i);
-        }
-        System.out.println("Exit");
+    @Test
+    public void whenPrtMenu() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserActions[] { action });
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("     Menu.")
+                .add("0. Stub action")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+    @Test
+    public void whenCheckOutputOnShowAction() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        ShowAction act = new ShowAction();
+        act.execute(new StubInput(new String[] {}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("#" + item.getId() + ", " + item.getName())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+    @Test
+    public void whenCheckOutputOnFindByNameAction() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        FindByNameAction act = new FindByNameAction();
+        act.execute(new StubInput(new String[] {"fix bug"}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("#" +item.getId() + ", " + item.getName())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
     }
 }
