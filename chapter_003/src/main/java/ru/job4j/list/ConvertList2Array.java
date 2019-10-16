14b22cfa84abd78430fac9782eb791016b0a63ea
Mikhail Cherednichenko <28explay@gmail.com>
2019-10-16 16:50:46 +0300

    3. Конвертация ArrayList в двухмерный массив[#170266]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -0,0 +1,26 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public int[][] toArray(List<Integer> list, int rows) {
+        int count = 1;
+        int rst = list.size() - rows;
+        while (rst > 0) {
+            rst -= rows;
+            count++;
+        }
+        int cells = count;
+        int[][] array = new int[rows][cells];
+        int i = 0;
+        int j = 0;
+        for (int cellValue : list) {
+            array[i][j++] = cellValue;
+            if (j % cells == 0) {
+                j = 0;
+                i++;
+        }
+        }
+        return array;
+    }
+}
