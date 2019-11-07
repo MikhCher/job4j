40489f4261dc0c1e6700f7c439d88de4e8b4aef5
Mikhail Cherednichenko <28explay@gmail.com>
2019-11-07 19:08:56 +0300

    Refactoring

--- chapter_004/src/main/java/ru/job4j/lambda/DiapasonCounter.java
+++ chapter_004/src/main/java/ru/job4j/lambda/DiapasonCounter.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 
 public class DiapasonCounter {
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
-        List <Double> result = new ArrayList<>();
+        List<Double> result = new ArrayList<>();
         for (int index = start; index < end; index++) {
             result.add(func.apply((double) index));
         }
