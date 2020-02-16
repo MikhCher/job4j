75d24a99b1ca9d53dfd788591de6c83344b185c6
Mikhail Cherednichenko <28explay@gmail.com>
2020-02-16 22:15:59 +0300

    2. Не перекрывать equals hashCode[#203256]

---
+++ chapter_005/src/test/java/ru/job4j/set/DefaultMapTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.set;
+
+import org.junit.Test;
+import ru.job4j.map.User;
+
+public class DefaultMapTest {
+
+    @Test
+    public void whenMapContainsTwoUsersWithSameFields() {
+        DefaultMap def = new DefaultMap();
+        User first = new User("Mikhail", 0, null);
+        User second = new User("Mikhail", 0, null);
+
+        def.add(first, 1);
+        def.add(second, 2);
+        def.printMap();
+    }
+
+}
