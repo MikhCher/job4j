44d221842179e4a83ad0bfe2e3cd05040f2e653c
Mikhail Cherednichenko <28explay@gmail.com>
2020-04-15 23:58:14 +0300

    2. Тестирование IO[#203286]

--- chapter_006/src/test/java/ru/job4j/io/AnalysisTest.java
+++ chapter_006/src/test/java/ru/job4j/io/AnalysisTest.java
@@ -1,24 +1,37 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 public class AnalysisTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void whenServerDoesNotWorkTwoTimes() {
+    public void whenServerDoesNotWorkTwoTimes() throws IOException {
         Analysis analysis = new Analysis();
-        analysis.unavailable("D:\\projects\\Job4j\\unavailable.csv", "D:\\projects\\Job4j\\target.txt");
-        try (BufferedReader read = new BufferedReader(new FileReader("D:\\projects\\Job4j\\target.txt"))) {
-            assertThat(read.readLine(), is("10:58:01;10:59:01"));
-            assertThat(read.readLine(), is("11:01:02;11:03:02"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        File source = folder.newFile("unavailable.csv");
+        File target = folder.newFile("target.txt");
+        Files.write(source.toPath(), List.of(
+                "200 10:56:01",
+                "200 10:57:01",
+                "400 10:58:01",
+                "200 10:59:01",
+                "500 11:01:02",
+                "400 11:02:02",
+                "200 11:03:02"
+        ));
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        List<String> result = Files.readAllLines(target.toPath());
+        assertThat(result.get(0), is("10:58:01;10:59:01"));
+        assertThat(result.get(1), is("11:01:02;11:03:02"));
     }
 }
